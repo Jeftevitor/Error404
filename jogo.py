1eecb5162312946976c81747253101d971e76e14
JEFTE VITOR DANTAS SILVA <20251101110010@ifrn.local>
2026-08-24 09:08:33 -0300

    romero msc

--- jogo.py
+++ jogo.py
@@ -41,7 +41,7 @@ class Jogo:
             {
                 "nome": "Romerito",
                 "arquivo": "Assets/Arquivos_txt/fase3.txt",
-                "musica": "Assets/Music/Romerito.ogg",
+                "musica": "Assets/Music/ROMERITO(Nuevayol- bad bunny).ogg",
                 "desbloqueada": False
             }
         ]
